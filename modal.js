8f76d4612dac0842ee823c12dfd635e87eb4a654
e.buckel@orange.fr <e.buckel@orange.fr>
2021-02-05 14:15:42 +0100

    js_ok

--- modal.js
+++ modal.js
@@ -32,65 +32,148 @@ function closeModal() {
 
 //Formulaire : sous-parties
 
-document.getElementById('first').addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
-      //Field contains valid data.
-  } else {
-      //Field contains invalid data.
-  }
-}, false);
-
-first.addEventListener('keyup', function() {
-  if (first.checkValidity()) {
-    first_error.innerHTML = "";
-  } else {
-    first_error.innerHTML = "Veuillez choisir un prénom plus long d'au moins 2 charatères";
-  }
-});
-
-last.addEventListener('keyup', function() {
-  if (last.checkValidity()) {
-    last_error.innerHTML = "";
-  } else {
-    last_error.innerHTML = "Veuillez choisir un nom plus long d'au moins 2 charatères";
-  }
-});
-
-email.addEventListener('keyup', function() {
-  if (email.checkValidity()) {
-    email_error.innerHTML = "";
-  } else {
-    email_error.innerHTML = "Veuillez entrer une adresse mail valide";
-  }
-});
-
-// birthdate.addEventListener('keyup', function() {
-//   if (birthdate.checkValidity()) {
-//     birthdate_error.innerHTML = "";
+// first.addEventListener('keyup', function() {
+//   if (first.checkValidity()) {
+//     first_error.innerHTML = "";
 //   } else {
-//     birthdate_error.innerHTML = "Veuillez entrer une adresse mail valide";
+//     first_error.innerHTML = "Veuillez choisir un prénom plus long d'au moins 2 charatères";
 //   }
 // });
 
-quantity.addEventListener('keyup', function() {
-  if (quantity.checkValidity()) {
+
+const first = document.getElementById("first");
+const first_error = document.getElementById("first_error");
+
+first.addEventListener("keyup", error_help_first);
+
+function error_help_first() {
+  if (first.value.length>1) {
+    first_error.innerHTML = "";
+    first.classList.remove("class_error");
+  } else {
+    first_error.innerHTML = "Veuillez choisir un prénom plus long d'au moins 2 charatères";
+  }
+};
+
+
+const last = document.getElementById("last");
+const last_error = document.getElementById("last_error");
+
+last.addEventListener('keyup', error_help_last);
+
+function error_help_last() {
+  if (last.value.length>1) {
+    last_error.innerHTML = "";
+    last.classList.remove("class_error");
+  } else {
+    last_error.innerHTML = "Veuillez choisir un nom plus long d'au moins 2 charatères";
+  }
+};
+
+const email = document.getElementById("email");
+const email_error = document.getElementById("email_error");
+
+email.addEventListener('keyup', error_help_email);
+function error_help_email() {
+  var pattern = /.+@.+\..+/ ;
+  var adress = pattern.exec(email.value);
+  if (email.value == adress && email.value != "") {
+    email_error.innerHTML = "";
+    email.classList.remove("class_error");
+  } else {
+    email_error.innerHTML = "Veuillez entrer une adresse mail valide";
+  }
+};
+
+const birthdate = document.getElementById("birthdate");
+const birthdate_error = document.getElementById("birthdate_error");
+
+birthdate.addEventListener('input', error_help_birthdate);
+function error_help_birthdate() {
+  if (    !isNaN(new Date(birthdate.value).getTime()) &&
+          new Date(birthdate.value).getTime() < new Date().getTime() 
+          // && ( new Date().getTime() - new Date(birthdate.value).getTime() ) / (1000*3600*24*365.25) > 18
+        ) {
+    birthdate_error.innerHTML = "";
+    birthdate.classList.remove("class_error");
+  } else {
+    birthdate_error.innerHTML = "Veuillez entrer une date valide";
+  }
+};
+
+const quantity = document.getElementById("quantity");
+const quantity_error = document.getElementById("quantity_error");
+
+quantity.addEventListener('keyup', error_help_quantity);
+function error_help_quantity() {
+  if ( !isNaN(quantity.value) && quantity.value !== "") {
     quantity_error.innerHTML = "";
+    quantity.classList.remove("class_error");
   } else {
     quantity_error.innerHTML = "Veuillez entrer un chiffre valide";
   }
-});
+};
 
-checkbox1.addEventListener('clic', function() {
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox1_error = document.getElementById("checkbox1_error");
+
+checkbox1.addEventListener('change', error_help_checkbox1);
+  function  error_help_checkbox1() {
   if (checkbox1.checked) {
     checkbox1_error.innerHTML = "";
+    checkbox1_label.classList.remove("class_error");
   } else {
     checkbox1_error.innerHTML = "Cette case doit être cochée";
   }
-});
+};
 
-//function validate() {
+function checkMyForm() {
+var form_valid = true;
 
-//if ( !first.validity.valid ) { first_error.innerHTML = "ERREUR_test" }
+//Check if firstname ok
+if (first.value.length<2)  {first.classList.add("class_error");form_valid = false;};
+
+//Check if lastname ok
+if (last.value.length<2)   {last.classList.add("class_error");form_valid = false;};
+
+//Check if email ok
+var pattern = /.+@.+\..+/ ;
+var adress = pattern.exec(email.value);
+if (email.value != adress || email.value == "") {email.classList.add("class_error");form_valid = false;};
+
+//Check if birthdate valid
+if (  (isNaN(new Date(birthdate.value).getTime()) ||
+       new Date(birthdate.value).getTime() > new Date().getTime()  ) 
+      // || ( new Date().getTime() - new Date(birthdate.value).getTime() ) / (1000*3600*24*365.25) < 18
+      )
+
+    {birthdate.classList.add("class_error");
+      form_valid = false;};
+
+//Check if quantity is ok
+if ( isNaN(quantity.value) || quantity.value == "") {quantity.classList.add("class_error");form_valid = false;};
+
+//Check if checkbox checked
+if (!checkbox1.checked) {checkbox1_label.classList.add("class_error");form_valid = false;};
+
+if (form_valid){ return true; };
+return false;
+
+};
 
 
-//}
+function validate(e) {
+e.preventDefault();
+
+error_help_first();
+if ( checkMyForm() ) { 
+  document.getElementById("test").innerHTML = `
+  Merci !
+  <br>
+  Votre participation a bien été enregistrée.
+`;
+ } else {
+ };
+
+
+};
